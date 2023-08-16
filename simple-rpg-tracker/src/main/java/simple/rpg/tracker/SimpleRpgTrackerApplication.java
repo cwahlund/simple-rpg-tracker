d7f4112346e8c5ed1d466610d192a6e83f6a2bf9
cwahlund <wahlund@icloud.com>
2023-08-15 18:10:25 -0700

    Maven project and Spring launcher setup

---
+++ simple-rpg-tracker/src/main/java/simple/rpg/tracker/SimpleRpgTrackerApplication.java
@@ -0,0 +1,13 @@
+package simple.rpg.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRpgTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRpgTrackerApplication.class, args);
+	}
+
+}
